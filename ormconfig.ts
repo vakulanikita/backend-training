22787c631abb8ef97f2e693aba33e2ea3cd880f8
Nikita <nik.vakula@bk.ru>
2022-07-27 19:36:26 +0300

    deploy settings

--- ormconfig.ts
+++ ormconfig.ts
@@ -20,7 +20,7 @@ module.exports = {
   "type": "postgres",
   "host": "abul.db.elephantsql.com",
   "port": 5432,
-  "entities": ["src/entity/post.entity"],
+  "entities": ["src/entity/post.entity", 'dist/src/entity/*.js'],
   "username": "rtyooyqx",
   "password": "mwxdurMGLqXVeYHJpum1OO5ALHb5PZ1H",
   "database": "rtyooyqx",
